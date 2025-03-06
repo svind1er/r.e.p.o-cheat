c6884a36313b0d251ee09e25e4d3c4dde4fc5f63
svindler <83125110+svind1er@users.noreply.github.com>
2025-03-06 05:36:47 +0100

    added method for randomizing color on click -- can experiment

---
+++ r.e.p.o cheat/Cheats/playerColor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using UnityEngine;
+
+namespace r.e.p.o_cheat
+{
+    internal class playerColor
+    {
+        public static void colorRandomizer()
+        {
+            var colorControllerType = Type.GetType("PlayerAvatar, Assembly-CSharp");
+            if (colorControllerType != null)
+            {
+                cheats.Log("colorControllerType found.");
+
+                var colorControllerInstance = Helper.FindObjectOfType(colorControllerType);
+                if (colorControllerInstance != null)
+                {
+                    cheats.Log("colorControllerInstance found. ");
+                    var playerSetColorMethod = colorControllerType.GetMethod("PlayerAvatarSetColor");
+                    if (playerSetColorMethod != null)
+                    {
+                        var colorIndex = new Random().Next(0, 30);
+                        playerSetColorMethod.Invoke(colorControllerInstance, new object[] { colorIndex });
+                    }
+                    else
+                    {
+                        cheats.Log("PlayerAvatarSetColor method not found in PlayerAvatar.");
+                    }
+                }
+                else
+                {
+                    cheats.Log("colorControllerInstance not found.");
+                }
+            }
+            else
+            {
+                cheats.Log("colorControllerType not found.");
+            }
+        }
+    }
+}
